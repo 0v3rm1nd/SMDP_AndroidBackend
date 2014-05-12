6f4278ac705a44a151b8abcd02a5d80d874b71d7
Martin Ivanov <maiva1337@gmail.com>
2014-05-12 15:12:28 +0200

    Removed old sqlite approach packages + helpers

--- src/com/smdp/surveytoandroid/questionstructure/DichotomousQuestion.java
+++
@@ -1,9 +0,0 @@
-package com.smdp.surveytoandroid.questionstructure;
-
-public class DichotomousQuestion extends Question implements Questionable{
-
-	public DichotomousQuestion(String name, String question, boolean required) {
-		super(name, question, required);
-		// TODO Auto-generated constructor stub
-	}
-}
